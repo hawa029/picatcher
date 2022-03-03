ab78096c276830ce84350b49a5d1c283bfe694b8
hawa029 <91275340+hawa029@users.noreply.github.com>
2022-03-03 18:46:19 +0100

    add blog title

--- utils/global-data.js
+++ utils/global-data.js
@@ -1,13 +1,13 @@
 export const getGlobalData = () => {
   const name = process.env.BLOG_NAME
     ? decodeURI(process.env.BLOG_NAME)
-    : 'Jay Doe';
+    : 'Bienvenue sur le site web de ';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : 'Next.js Blog Theme';
+    : 'Charles CANTIN - Photographe';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
-    : 'All rights reserved.';
+    : '2022 - Tous droits réservés.';
 
   return {
     name,
