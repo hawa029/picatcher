34e6ce44412cdbfb5af429d0c88a9ded824c1815
hawa029 <91275340+hawa029@users.noreply.github.com>
2022-03-04 10:14:23 +0100

    contact form built

--- pages/contact.js
+++ pages/contact.js
@@ -7,9 +7,46 @@ export default function Galleries() {
     <Layout>
       <Header />
       <main className="w-full">
-        <h1 className="text-3xl lg:text-5xl text-center mb-12">
-          Tarifs et prestations
-        </h1>
+        <h1 className="text-3xl lg:text-5xl text-center mb-12">Contact</h1>
+
+        <section class="max-w-4xl p-6 mx-auto bg-white rounded-md shadow-md dark:bg-gray-800 mt-20">
+          <h1 class="text-xl font-bold text-white capitalize dark:text-white">
+            Formulaire de contact
+          </h1>
+          <form>
+            <div class="grid grid-cols-1 gap-6 mt-4 sm:grid-cols-2">
+              <div>
+                <label
+                  class="text-gray-700 dark:text-gray-200"
+                  for="emailAddress"
+                >
+                  Email Address
+                </label>
+                <input
+                  id="emailAddress"
+                  type="email"
+                  class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
+                />
+              </div>
+            </div>
+            <div>
+              <label class="text-gray-700 dark:text-gray-200" for="message">
+                Votre message
+              </label>
+              <textarea
+                id="textarea"
+                type="textarea"
+                class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
+              ></textarea>
+            </div>
+
+            <div class="flex justify-end mt-6">
+              <button class="px-6 py-2 leading-5 text-white transition-colors duration-200 transform bg-gray-700 rounded-md hover:bg-gray-600 focus:outline-none focus:bg-gray-600">
+                Envoyer
+              </button>
+            </div>
+          </form>
+        </section>
       </main>
 
       <Footer />
