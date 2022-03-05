d0f5f48cb146880caf3b7aadf260bcb2c0fafaec
hawa029 <91275340+hawa029@users.noreply.github.com>
2022-03-05 11:11:26 +0100

    baptême album start changes

--- pages/Albums/BaptemeAlbum.js
+++ pages/Albums/BaptemeAlbum.js
@@ -1,44 +1,73 @@
-import * as React from 'react';
-import Card from '@mui/material/Card';
-import CardMedia from '@mui/material/CardMedia';
-import Grid from '@mui/material/Grid';
-import Container from '@mui/material/Container';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
+import Footer from '../../components/Footer';
+import Navigation from '../../components/Navigation';
+import Layout, { GradientBackground } from '../../components/Layout';
+import Image from 'next/image';
 
-const theme = createTheme();
+export default function Bébé() {
+  return (
+    <Layout>
+      <Navigation />
+      <main className="w-full">
+        <h1 className="text-3xl lg:text-5xl text-center mb-5">
+          Photos de bébés
+        </h1>
+        <div className="2xl:container 2xl:mx-auto md:py-12 lg:px-20 md:px-6 py-9 px-4">
+          <div className="text-center">
+            <p className="font-normal text-base leading-6 dark:text-gray-400 text-gray-600 mt-4 lg:w-10/12 md:w-9/12 mx-auto">
+              Les photos sont classées par catégories. Cliquez sur la catégories
+              de votre choix pour y accéder
+            </p>
+          </div>
+          <div className="grid lg:grid-cols-2 sm:grid-cols-2 grid-cols-1 lg:grap-8 md:gap-6 gap-4 mt-10">
+            <div className="relative group">
+              <Image
+                src="/bébé.jpg"
+                alt="Bébé"
+                height={400}
+                width={600}
+                className="lg:block hidden w-full"
+              />
+            </div>
+            <div className="relative group">
+              <Image
+                src="/bébé.jpg"
+                alt="Bébé"
+                height={400}
+                width={600}
+                className="lg:block hidden w-full"
+              />
+            </div>
+            <div className="relative group">
+              <Image
+                src="/bébé.jpg"
+                alt="Bébé"
+                height={400}
+                width={600}
+                className="lg:block hidden w-full"
+              />
+            </div>
+            <div className="relative group">
+              <Image
+                src="/bébé.jpg"
+                alt="Bébé"
+                height={400}
+                width={600}
+                className="lg:block hidden w-full"
+              />
+            </div>
+          </div>
+        </div>
+      </main>
 
-export default function Album() {
-	return (
-		<ThemeProvider theme={theme}>
-			<main>
-				{/* Hero unit */}
-
-				<Container
-					style={{
-						py: 1,
-						display: 'flex',
-						flexDirection: 'row',
-					}}>
-					<Grid xs={12} lg={6} md={6} sx={{ p: 2 }}>
-						<Card>
-							<CardMedia
-								component='img'
-								image='https://images.unsplash.com/photo-1566516171511-1c411a59c8ba?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80'
-								alt='Baptême'
-							/>
-						</Card>
-					</Grid>
-					<Grid xs={12} lg={6} md={6} sx={{ p: 2 }}>
-						<Card>
-							<CardMedia
-								component='img'
-								image='https://images.unsplash.com/photo-1543717095-8a7cf6e5e9f5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80'
-								alt='Baptême'
-							/>
-						</Card>
-					</Grid>
-				</Container>
-			</main>
-		</ThemeProvider>
-	);
+      <Footer />
+      <GradientBackground
+        variant="large"
+        className="fixed top-20 opacity-40 dark:opacity-60"
+      />
+      <GradientBackground
+        variant="small"
+        className="absolute bottom-0 opacity-20 dark:opacity-10"
+      />
+    </Layout>
+  );
 }
