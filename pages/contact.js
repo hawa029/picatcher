4bcf0aa9d33bbdc414aed72dc7f4ca50ed76d5f2
hawa029 <91275340+hawa029@users.noreply.github.com>
2022-03-07 17:22:59 +0100

    adding name at the input form

--- pages/contact.js
+++ pages/contact.js
@@ -32,6 +32,7 @@ export default function Galleries({ posts, globalData }) {
                 <input
                   id="emailAddress"
                   type="email"
+                  name="email"
                   className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
                 />
               </div>
@@ -46,13 +47,17 @@ export default function Galleries({ posts, globalData }) {
                 <textarea
                   id="textarea"
                   type="textarea"
+                  name="message"
                   rows={6}
                   className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
                 ></textarea>
               </div>
 
               <div className="flex justify-end mt-6">
-                <button className="px-6 py-2 leading-5 text-white transition-colors duration-200 transform bg-gray-700 rounded-md hover:bg-gray-600 focus:outline-none focus:bg-gray-600">
+                <button
+                  className="px-6 py-2 leading-5 text-white transition-colors duration-200 transform bg-gray-700 rounded-md hover:bg-gray-600 focus:outline-none focus:bg-gray-600"
+                  type="submit"
+                >
                   Envoyer
                 </button>
               </div>
